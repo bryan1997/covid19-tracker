c3e2afa97b3f0fa9dee6206bab3acd778e39269a
Jun Zhe <hoojunzhe@gmail.com>
2021-06-10 22:15:09 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Covid-19 Tracker
+
+This is a Covid-19 tracker project developed with Vue.js
+
+## API
+
+This project uses the [covid19api.com](https://covid19api.com/) by [Kyle Redelinghuys]
+
+
+### Setup
+
+```
+npm install
+```
+
+### Run
+
+```
+npm run serve
+```
+
+### Build
+
+```
+npm run build
+```
---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <header class="text-center bg-green-700 text-white p-4 mb-10">
+    <div class="text-3xl md:text-5xl font-bold mb-3">
+      <i class="fa fa-viruses"></i> Covid-19 Tracker
+    </div>
+
+    <p>
+      API by
+      <a class="text-blue-300" href="https://covid19api.com" target="_blank">covid19api.com</a>
+    </p>
+    <p>
+      Developed by
+      <a class="text-blue-300" href='https://www.linkedin.com/in/hoo-jun-zhe-0960b1192/' target="_blank">Hoo Jun Zhe</a>
+    </p>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+};
+</script>
